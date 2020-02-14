cbc279e87259d4913ca86653b3817b98a0ecc488
Student <example@example.com>
2020-02-14 13:52:30 -0800

    Configure CSS modules

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -4,6 +4,12 @@ import PropTypes from 'prop-types';
 function Keg(props){
   return (
     <div>
+        <style global jsx>{`
+          div {
+            background-color: red;
+          }
+        `}</style>
+        
       <h3>{props.style} - {props.brand} - {props.alcoholVolume}</h3>
       <p><em>{props.price}</em></p>
       <hr/>
@@ -13,7 +19,7 @@ function Keg(props){
 Keg.propTypes = {
   style: PropTypes.string.isRequired,
   brand: PropTypes.string.isRequired,
-  alcoholVolume: PropTypes.string.isRequired,
+  alcoholVolume: PropTypes.number.isRequired,
   price: PropTypes.number.isRequired
 };
 
--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import Keg from './Keg';
 
+var masterKegList = [
+  {
+    style: 'Dutch Pale Lager',
+    brand: 'Heineken',
+    alcoholVolume:'5%',
+    price: '6'
+  },
+  {
+    style: 'Pilsner',
+    brand: 'Stella Artois',
+    alcoholVolume:'4.8%',
+    price: '5'
+  },
+];
+
 function KegList(){
   return (
-    <Keg
-      style="Dutch Pale Lager"
-      brand="Heineken"
-      price="6.00"
-      alcoholVolume="5%" />
+    <div>
+      <hr />
+      {masterKegList.map((keg, index) =>
+        <Keg style={keg.style} brand={keg.brand} alcoholVolume={keg.alcoholVolume} price={keg.price} key={index}/>)}
+    </div>
   );
 }
 
